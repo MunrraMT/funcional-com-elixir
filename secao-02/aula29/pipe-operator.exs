924ba6fd5f396ab60848a04c676680519756364e
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-02-12 23:05:51 -0400

    29. Pipe Operator

---
+++ secao-02/aula29/pipe-operator.exs
@@ -0,0 +1,10 @@
+str_len = String.length("Maria")
+IO.puts(str_len) # 5
+
+IO.puts(String.length("Maria")) # 5
+
+"Olá!" |> String.length |> IO.puts # 4
+
+"Olá!"
+  |> String.length
+  |> IO.puts # 4

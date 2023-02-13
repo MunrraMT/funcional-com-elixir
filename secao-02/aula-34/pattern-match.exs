a2315d407e82a52d414f475111f580d154152fdc
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-02-13 00:34:33 -0400

    34. Fazendo Matching de Strings

---
+++ secao-02/aula-34/pattern-match.exs
@@ -0,0 +1,5 @@
+IO.puts("Content-Type: " <> content_type = "Content-Type: text/html; charset=UTF-8")
+IO.puts(content_type)
+# Adiciona à variável "content_type" a string sem o conteúdo da string que fez match
+# retornando:
+# text/html; charset=UTF-8
